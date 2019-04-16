9b5693d9adae09e11a2d872349db0e7edc06a6a9
zhouyunxiang <1509092328@qq.com>
2019-04-16 17:31:15 +0800

    logger集成

--- zyx-springboot/src/main/java/com/ideal/zyxspringboot/listener/AyUserListener.java
+++ zyx-springboot/src/main/java/com/ideal/zyxspringboot/listener/AyUserListener.java
@@ -2,6 +2,8 @@ package com.ideal.zyxspringboot.listener;
 
 import com.ideal.zyxspringboot.model.AyUser;
 import com.ideal.zyxspringboot.service.AyUserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.annotation.Resource;
@@ -10,8 +12,11 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import java.util.List;
 
+
+
 @WebListener
 public class AyUserListener implements ServletContextListener {
+    Logger logger = LogManager.getLogger(this.getClass());
     @Resource
     private RedisTemplate redisTemplate;
     @Resource
@@ -27,13 +32,17 @@ public class AyUserListener implements ServletContextListener {
         redisTemplate.delete(All_USER);
         //存入缓存
         redisTemplate.opsForList().leftPushAll(All_USER,ayUserList);
-        System.out.println("ServletContext上下文初始化");
+
         //查看缓存中的数据，真实项目中不需要
         //redisTemplate.opsForList().range获取指定区间的值
         List<AyUser> queryUserList = redisTemplate.opsForList().range(All_USER,0,-1);
+        //System.out.println("ServletContext上下文初始化");
+        logger.info("ServletContext上下文初始化");
+        logger.info("缓存中目前的用户输有："+queryUserList.size()+"人");
     }
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent){
-        System.out.println("ServletContext 上下文销毁");
+        //System.out.println("ServletContext 上下文销毁");
+        logger.info("ServletContext 上下文销毁");
     }
 }
